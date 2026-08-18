19610588d03f71afb6aaf75ce6d1e7d42beedfca
alessio-aesthetic <alessi.ola.rocchia@gmail.com>
2026-08-18 22:07:18 +0200

    Aggiorna indirizzo Salerno

--- src/data/site.ts
+++ src/data/site.ts
@@ -5,7 +5,7 @@ export const site = {
     "tel":  "+390891803547",
     "city":  "Salerno",
     "name":  "Carroattrezzi Salerno",
-    "address":  "SS 18 Tirrena Inferiore, 157 - 84131 Salerno SA",
+    "address":  "Via Davide Galdi, 68 - 84129 Salerno SA",
     "title":  "Carroattrezzi a Salerno 24H | Soccorso stradale",
     "phone":  "089 180 3547",
     "email":  "assistenza@carroattrezzisalerno.it"
